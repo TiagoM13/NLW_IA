979c329435c70a05813e601cae89ba7493aa8fa1
TiagoM13 <thyagomotha2000@gmail.com>
2023-09-18 21:07:13 -0300

    feat(routes): add routes to server

--- upload-ai-server/src/server.ts
+++ upload-ai-server/src/server.ts
@@ -1,10 +1,13 @@
 import { fastify } from "fastify";
+import { getAllPromptsRoutes } from "./routes/get-all-prompts";
+import { uploadVideoRoutes } from "./routes/upload-video";
+import { createTranscriptionRoute } from "./routes/create-transcription";
 
 const app = fastify()
 
-app.get('/', () => {
-  return 'Hello Word'
-})
+app.register(getAllPromptsRoutes)
+app.register(uploadVideoRoutes)
+app.register(createTranscriptionRoute)
 
 app.listen({
   port: 3333,
